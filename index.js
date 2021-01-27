9b054b62367b197d72d5ecc6f39381200cca988d
yuchida-tamu <yutaside1992@gmail.com>
2021-01-27 22:51:19 +0900

    mongodb dev setup

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.on("open", () => {
+  console.log("DB connected!!!");
+});
 
 app.use(cors());
 /*
