af6d0615b2a77513cff70d48a077267363ec3688
yuchida-tamu <yutaside1992@gmail.com>
2021-01-28 23:21:31 +0900

    add Put/Delete User

--- api/user.js
+++ api/user.js
@@ -8,8 +8,17 @@ const mongoose = require("mongoose");
 
 const UserModel = mongoose.model("User");
 
-router.route("/").get((req, res) => {
-  res.send("User API...");
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await UserModel.find();
+    res
+      .status(200)
+      .json({ status: "SUCCESS", msg: "fetched users", user: users });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ status: "FAIL", msg: "could not fetch users", error: err });
+  }
 });
 
 router.route("/new").post(async (req, res) => {
@@ -64,23 +73,114 @@ router.route("/new").post(async (req, res) => {
   }
 });
 
-router.route("/:id").get(async (req, res) => {
-  const { id } = req.params;
-  /* validate input */
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(300).json({ status: "FAIL", msg: "invalid id format" });
-  }
+router
+  .route("/:id")
+  .get(async (req, res) => {
+    const { id } = req.params;
+    /* validate input */
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(300).json({ status: "FAIL", msg: "invalid id format" });
+    }
 
-  try {
-    const fetchedUser = await UserModel.findById(id);
-    res
-      .status(200)
-      .json({ status: "SUCCESS", msg: "fetched the user", user: fetchedUser });
-  } catch (err) {
-    res
-      .status(500)
-      .json({ status: "FAIL", msg: "could not fetch the user", error: err });
-  }
-});
+    try {
+      const fetchedUser = await UserModel.findById(id);
+      res.status(200).json({
+        status: "SUCCESS",
+        msg: "fetched the user",
+        user: fetchedUser,
+      });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ status: "FAIL", msg: "could not fetch the user", error: err });
+    }
+  })
+  .put(async (req, res) => {
+    const { id } = req.params;
+    const { username, first_name, last_name, email, password } = req.body;
+    /* Input validation */
+    if (!username)
+      res.status(300).json({ status: "FAIL", msg: "username is missing" });
+    if (!first_name)
+      res.status(300).json({ status: "FAIL", msg: "first_name is missing" });
+    if (!last_name)
+      res.status(300).json({ status: "FAIL", msg: "last_name is missing" });
+    if (!email)
+      res.status(300).json({ status: "FAIL", msg: "email is missing" });
+    if (!password)
+      res.status(300).json({ status: "FAIL", msg: "password is missing" });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(300).json({ status: "FAIL", msg: "invalid id format" });
+    }
+
+    const findUsername = await UserModel.findOne({ username: username }).exec();
+    /*Check if a new username is already used for other users */
+    const findEmail = await UserModel.findOne({ email: email }).exec();
+    if (findUsername) {
+      if (findUsername._id != id) {
+        return res.status(300).json({
+          status: "FAIL",
+          msg: "user with the same username already exists",
+        });
+      }
+    }
+    /*Check is a new email is already used for other users */
+    if (findEmail) {
+      console.log(id);
+      console.log(findEmail._id);
+      if (findEmail._id != id) {
+        return res.status(300).json({
+          status: "FAIL",
+          msg: "the email is already used",
+        });
+      }
+    }
+    const options = { new: true, useFindAndModify: false };
+
+    try {
+      const updates = {
+        username,
+        first_name,
+        last_name,
+        email,
+        password,
+      };
+
+      const updatedUser = await UserModel.findOneAndUpdate(
+        { _id: id },
+        { ...updates },
+        options
+      ).exec();
+
+      res.status(200).json({
+        status: "SUCCESS",
+        msg: "updated the user",
+        user: updatedUser,
+      });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ status: "FAIL", msg: "could not update the user", error: err });
+    }
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(300).json({ status: "FAIL", msg: "invalid id format" });
+    }
+
+    try {
+      const deleted = UserModel.findOneAndRemove({ _id: id }).exec();
+      res.status(200).json({
+        status: "SUCCESS",
+        msg: "deleted the user",
+        user: deleted,
+      });
+    } catch (err) {
+      res
+        .status(500)
+        .json({ status: "FAIL", msg: "could not delete the user", error: err });
+    }
+  });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const UserSchema = new mongoose.Schema({
   username: { type: String },
   first_name: { type: String },
   last_name: { type: String },
+  email: { type: String },
   password: { type: String },
   visiting_events: { type: [EventList] },
   visiting_places: { type: [PlaceList] },
