5100b96e86900a887cab4d235b3efd45bcb86b46
yuchida-tamu <yutaside1992@gmail.com>
2021-02-11 09:32:34 +0900

    add authentication

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -87,6 +88,22 @@ app.use(cors());
  * parses request body (json)
  */
 app.use(bodyParser.json());
+//configure cookie
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30days in milliseconds
+    keys: ["djahdavh12ui32hoijdhiahaaihida"], //Delete
+  })
+);
+//enable cookie
+app.use(passport.initialize());
+app.use(passport.session());
+/*
+ * auth
+ */
+require("./config/passport");
+app.use(bodyParser.urlencoded({ extended: false }));
+require("./api/auth")(app);
 
 /*
  * user api service
