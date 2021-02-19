fb26e3a20a648f14824f3caa2f52f6c1d9a086cb
yuchida-tamu <yutaside1992@gmail.com>
2021-02-19 23:18:39 +0900

    event-dashboard basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
 import "./App.css";
-import photo from "./test.jpg";
+
+import EventDashboard from "./containers/EventDashboard";
 
 function App() {
   return (
     <div className="App">
       <header>
         <nav>
-          <div class="nav-wrapper indigo darken-4">
-            <a href="#" class="brand-logo">
+          <div className="nav-wrapper indigo darken-4">
+            <a href="#" className="brand-logo">
               DOKOIKO
             </a>
-            <ul class="right hide-on-med-and-down">
+            <ul className="right hide-on-med-and-down">
               <li>
                 <a>Users</a>
               </li>
@@ -25,36 +26,7 @@ function App() {
         </nav>
       </header>
       <main className="Main row">
-        <div className="SideControl container col l3 indigo darken-2">
-          <div className="col l2 left MiniNav indigo darken-3">
-            <div>
-              <a className="btn-floating btn-medium  waves-effect waves-light indigo darken-4">
-                <i className="material-icons">person</i>
-              </a>
-              <a className="btn-floating btn-medium  waves-effect waves-light indigo darken-4">
-                <i className="material-icons">bookmark</i>
-              </a>
-              <a className="btn-floating btn-medium waves-effect waves-light indigo darken-4">
-                <i className="material-icons">event</i>
-              </a>
-              <a className="btn-floating btn-medium  waves-effect waves-light indigo darken-4">
-                <i className="material-icons">location_city</i>
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col l9 indigo MapContainer">
-          <div className="Map indigo"></div>
-          <div className="col l2 indigo darken-4 InfoPanel z-depth-3">
-            <div className="ImageBox">
-              <img className="ImageBox-Img responsive-img" src={photo} />
-            </div>
-            <div>
-              <div className="InfoPanel-Detail"></div>
-              <div className="InfoPanel-Detail"></div>
-            </div>
-          </div>
-        </div>
+        <EventDashboard />
       </main>
     </div>
   );
