daf1fc571fa113995386241c07f200a8e1b9e586
yuchida-tamu <yutaside1992@gmail.com>
2021-03-12 22:16:51 +0900

    implement a basic map

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { keys } from '../../keys/keys';
+const Map = ({ location }) => {
+  const { lat, lng } = location;
+  const [zoom, setZoom] = useState(15);
+
+  return (
+    <div style={{ height: '100%', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{
+          key: process.env.GOOGLE_MAP_KEY || keys.GOOGLE_MAP_KEY,
+        }}
+        defaultCenter={{ lat, lng }}
+        defaultZoom={zoom}
+        center={{ lat, lng }}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
