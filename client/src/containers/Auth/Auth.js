2a76d65c5a52feeb362dbf63f6de43b8c01e0373
yuchida-tamu <yutaside1992@gmail.com>
2021-03-28 14:23:17 +0900

    add create new user

--- client/src/containers/Auth/Auth.js
+++ client/src/containers/Auth/Auth.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { useCurrentUserContext } from '../../contexts/CurrentUserContext';
 import { userReducer } from '../../helpers/helper';
 
 const Auth = () => {
   const { user, setUser } = useCurrentUserContext();
+
+  //manage Auth state, signin or signup
+  const [isSignIn, setIsSignIn] = useState(true);
 
   const onSubmitHandler = event => {
     event.preventDefault();
@@ -27,21 +30,62 @@ const Auth = () => {
         window.alert('Failed to Login. Could not authenticate with the info');
       });
   };
-  return (
-    <div id='auth-card'>
+
+  const onSubmitToCreateHandler = event => {
+    event.preventDefault();
+    const {
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+      passwordconfirm,
+    } = event.target;
+    //check if passsword and password for confirmation match
+    if (password.value !== passwordconfirm.value)
+      return window.alert("Passwords you entered don't match");
+
+    const newUser = {
+      username: username.value,
+      first_name: firstname.value,
+      last_name: lastname.value,
+      email: email.value,
+      password: password.value,
+    };
+    console.log(newUser);
+
+    axios
+      .post('/api/v1/user/new', newUser)
+      .then(response => {
+        console.log(response.data);
+        const processedUser = userReducer(response.data.user, true);
+        setUser(processedUser);
+      })
+      .catch(err => {
+        console.error('Failed to create a new user: ', err);
+      });
+  };
+
+  const onToggleHandler = () => {
+    setIsSignIn(!isSignIn);
+  };
+
+  return isSignIn ? (
+    <div className='auth-card'>
       <h2>Welcome Back!</h2>
-      <form onSubmit={onSubmitHandler} id='auth-card__form'>
+
+      <form onSubmit={onSubmitHandler} className='auth-card__form'>
         <input
-          id='auth-username'
+          className='auth-username'
           type='text'
           name='username'
           placeholder='Your Username'
         />
 
-        <input id='auth-password' type='password' name='password' />
+        <input className='auth-password' type='password' name='password' />
 
         <button
-          id='auth-btn'
+          className='auth-btn'
           className='btn waves-effect waves-light'
           type='submit'
           name='action'
@@ -49,6 +93,63 @@ const Auth = () => {
           Sign In
         </button>
       </form>
+      <a href='#' onClick={onToggleHandler}>
+        Create a new account
+      </a>
+    </div>
+  ) : (
+    <div className='auth-card auth-card-new'>
+      <h2>Create Your Account</h2>
+
+      <form onSubmit={onSubmitToCreateHandler} className='auth-card__form'>
+        <label for='username'>Username</label>
+        <input
+          className='auth-username'
+          type='text'
+          name='username'
+          placeholder='Your Username'
+        />
+        <label for='firstname'>First</label>
+        <input
+          className='auth-firstname'
+          type='text'
+          name='firstname'
+          placeholder='First Name'
+        />
+        <label for='lastname'>Last</label>
+        <input
+          className='auth-lastname'
+          type='text'
+          name='lastname'
+          placeholder='Last Name'
+        />
+        <label for='email'>Email</label>
+        <input
+          className='auth-email'
+          type='email'
+          name='email'
+          placeholder='email@example.com'
+        />
+        <label for='password'>Password</label>
+        <input className='auth-password' type='password' name='password' />
+        <label for='passwordconfirm'>Confirm</label>
+        <input
+          className='auth-password-confirm'
+          type='password'
+          name='passwordconfirm'
+        />
+
+        <button
+          className='auth-btn btn waves-effect waves-light'
+          type='submit'
+          name='action'
+        >
+          Create
+        </button>
+      </form>
+      <a href='#' onClick={onToggleHandler}>
+        Sign in with different account
+      </a>
     </div>
   );
 };
